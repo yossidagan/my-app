9809a2960f91d9dd6a39efc3cdebbaecdc4dd8f3
yossi dagan <dagangm@gmail.com>
2020-03-13 14:38:00 +0200

    added mongodb connection + user schema

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const api = require('./server/routes/api')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const server = require("http").createServer(app);
+
+
 let io = require("socket.io")(server);
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use('/', api)
+
 app.get("/", (req, res) => res.send("Server up"));
+
 
 io.on("connection", socket => {
   console.log("A user has conneted");
@@ -15,5 +28,11 @@ io.on("connection", socket => {
     io.emit("RECEIVE_MESSAGE", msg);
   });
 });
+
+const db = require('./config/keys').mongoURI
+
+mongoose.connect(db)
+.then(() => console.log("MongoDB connected..."))
+.catch(err => console.log(err))
 
 server.listen(port, () => console.log(`listening on port ${port}!`));
