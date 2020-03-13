6cd65a99ba261ffc1cbd7cba5fcaad2cf03a33b5
yossi dagan <dagangm@gmail.com>
2020-03-13 04:34:19 +0200

    added on press event to input

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -3,50 +3,56 @@ import { useDispatch, useSelector } from "react-redux";
 import { sendMessage } from "../store/actions/chatsActions";
 import io from "socket.io-client";
 
-const ChatScreen = () => {
-  const [message, setMessage] = useState("");
-  const dispatch = useDispatch();
-  let chatsFromState = useSelector(state => state.chatsState.chats);
+        const ChatScreen = () => {
+          const [message, setMessage] = useState("");
+          const dispatch = useDispatch();
+          let chatsFromState = useSelector(state => state.chatsState.chats);
 
-  const socket = io("localhost:4000");
+          const socket = io("localhost:4000");
 
-  socket.on("RECEIVE_MESSAGE", msgReceived => {
-    console.log("msgReceived ", msgReceived);
-  });
-  
-  useEffect(() => {
-  
-  }, []);
+          socket.on("RECEIVE_MESSAGE", msgReceived => {
+            console.log("msgReceived ", msgReceived);
+          });
+          
+          useEffect(() => {
+          
+          }, []);
 
-  const handleChange = e => setMessage(e.target.value);
+          const handleChange = e => setMessage(e.target.value);
 
-  const handleSendMessage = async () => {
-    socket.emit("SEND_MESSAGE", message);
-    await dispatch(sendMessage(message));
-    setMessage("");
-    console.log(chatsFromState);
-  };
+          const handlePress = e => {
+            if (e.key === 'Enter') {
+            handleSendMessage()
+            }
+          }
 
-  return (
-    <div className="chatScreen">
-      <div className="">HI</div>
-      <input
-        value={message}
-        onChange={handleChange}
-        placeholder="Your message here..."
-      />
-      <div className="sendBtn" onClick={handleSendMessage}>
-        Send
-      </div>
-      <div className="messagesContainer">
-        {chatsFromState.map((chat, i) => (
-          <div className="messageBox" key={i}>
-            message : {chat.message}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+          const handleSendMessage = async e => {
+            socket.emit("SEND_MESSAGE", message);
+            await dispatch(sendMessage(message));
+            setMessage("");
+          };
 
-export default ChatScreen;
+          return (
+            <div className="chatScreen">
+              <div className="">HI</div>
+              <input
+                value={message}
+                onChange={handleChange}
+                onKeyPress={handlePress}
+                placeholder="Your message here..."
+              />
+              <div className="sendBtn" onClick={handleSendMessage}>
+                Send
+              </div>
+              <div className="messagesContainer">
+                {chatsFromState.map((chat, i) => (
+                  <div className="messageBox" key={i}>
+                    message : {chat.message}
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        };
+
+        export default ChatScreen;
--- src/store/reducers/chatsReducer.js
+++ src/store/reducers/chatsReducer.js
@@ -9,12 +9,12 @@ const chatsReducer = (state = initialState, action) => {
   console.log("message in reducer is ", message);
 
   let newMessage = {
-    control: "dddd",
     message: message
   };
 
   switch (action.type) {
     case SEND_MESSAGE:
+
       let newState = { ...state, chats: state.chats.push(newMessage) };
 
       return state;
