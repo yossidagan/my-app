64645df9c8b52fb501d64e3bb9f9d74655525755
yossi dagan <dagangm@gmail.com>
2020-03-20 21:51:48 +0200

    added auth reducer

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,55 @@
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
+} from "../actions/types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  isLoading: null,
+  user: null
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false
+      };
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT_SUCCESS:
+    case REGISTER_FAIL:
+        return {
+            ...state, 
+            token: null,
+            user: null,
+            isAuthenticated: false, 
+            isLoading: false
+        }
+  }
+};
+
+export default authReducer
